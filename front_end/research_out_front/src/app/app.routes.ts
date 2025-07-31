75fe7beefaa258cc6f767462e118092b4558e259
godfrey.tharage <godfrey.tharage@univen.ac.za>
2025-07-31 12:11:29 +0200

    front end init

--- front_end/research_out_front/src/app/app.routes.ts
+++ front_end/research_out_front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './components/login-component/login-component';
 import {HomeComponent} from './components/home-component/home-component';
+import {ResearchOutputComponent} from './components/research-output/research-output';
 
 
 export const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full' },
   {path:'login', component: LoginComponent},
   {path:'home', component: HomeComponent},
+  {path:'research', component:ResearchOutputComponent}
 ];
