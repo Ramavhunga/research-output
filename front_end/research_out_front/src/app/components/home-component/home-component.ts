22ad5f259ef4bb1692db67a100b4684dd6bd095e
godfrey.tharage <godfrey.tharage@univen.ac.za>
2025-08-15 09:31:09 +0200

    login layout

--- front_end/research_out_front/src/app/components/home-component/home-component.ts
+++ front_end/research_out_front/src/app/components/home-component/home-component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import {ResearchOutputComponent} from '../research-output/research-output';
-import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
   imports: [
-    ResearchOutputComponent,
-    RouterLink,
-    RouterLinkActive
+    ResearchOutputComponent
   ],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css'
